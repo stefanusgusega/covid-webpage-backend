354e37b5de6c4d3acd9a7d135ed19b3e66b4ee3a
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-06-19 11:18:59 +0700

    feat: implement apis

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 4000;
+const db = require('./queries');
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+
+app.get('/', (request, response) => {
+    response.json({
+        info: 'Node.js, Express, and Postgres API'
+    })
+})
+
+app.get('/first', db.getFirstTask);
+app.get('/second', db.getSecondTask);
+app.get('/third', db.getThirdTask);
+
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+})
---
+++ src/queries.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+
+const pg = require('pg');
+
+// Handle integer value that chanegd to string
+pg.types.setTypeParser(pg.types.builtins.INT8, (value) => {
+    return parseInt(value);
+});
+const Pool = pg.Pool
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'coviddatabase',
+    password: process.env.DB_PASSWORD,
+    port: 5432
+})
+
+const getFirstTask = (request, response) => {
+    pool.query('SELECT * FROM task_1_1', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getSecondTask = (request, response) => {
+    pool.query('SELECT * FROM task_1_2', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getThirdTask = (request, response) => {
+    pool.query('SELECT * FROM task_1_3', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getFirstTask,
+    getSecondTask,
+    getThirdTask
+}
